121249a5b234125cae48ddf431244beb65a63c93
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2014-03-12 16:55:25 -0300

    signin mockup returns message

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,12 @@
 class SessionController < ApplicationController
   def new
   end
+
+  def create
+    flash.now[:alert] = 'Invalid email or password'
+    render 'new'
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 JobsPageioApp::Application.routes.draw do
   get   'signup',                 :to => 'mockup#signup'
-  get   'signin',                 :to => 'session#new'
   get   'mockup',                 :to => 'mockup#index'
+
+  # TODO: Should be session or sessions
+  get   'signin',                 :to => 'session#new'
+  post  'signin',                 :to => 'session#create'
+  get   'signout',                :to => 'session#destroy'
 
   root 'session#new'
 end
