7a99f535d1d3bf7b850df40f4f25d0b432284a2f
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2014-03-12 11:55:47 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobsPageioApp::Application.config.secret_key_base = '93b40162576a0a44d0b695e3171b6e1172896f80a5e700e6b314dc3b3b9dfa9323160fbe6f810b5cfaa08bd9d20fcef91029eeb7dae4993edce7b80f32fb768e'
