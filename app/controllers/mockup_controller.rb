9d47775fb515f23460c37f27400575733650a318
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2014-03-13 16:59:41 -0300

    basic sidebar nav working

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -1,5 +1,46 @@
 class MockupController < ApplicationController
   def index
+    @panel = 'dashboard'
+  end
+
+  def activate
+    @panel = 'activate'
+    render 'index'
+  end
+
+  def dashboard
+    @panel = 'dashboard'
+    render 'index'
+  end
+
+  def jobs
+    @panel = 'jobs'
+    render 'index'
+  end
+
+  def replies
+    @panel = 'replies'
+    render 'index'
+  end
+
+  def activity
+    @panel = 'activity'
+    render 'index'
+  end
+
+  def team
+    @panel = 'team'
+    render 'index'
+  end
+
+  def customize
+    @panel = 'customize'
+    render 'index'
+  end
+
+  def plugins
+    @panel = 'plugins'
+    render 'index'
   end
 
   def signup
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 JobsPageioApp::Application.routes.draw do
-  get   'signup',                 :to => 'mockup#signup'
-  get   'mockup',                 :to => 'mockup#index'
+  get   'signup',                   :to => 'mockup#signup'
+
+  get   'mockup',                   :to => 'mockup#index'
+  get   'mockup/activate',          :to => 'mockup#activate'
+  get   'mockup/dashboard',         :to => 'mockup#dashboard'
+  get   'mockup/jobs',              :to => 'mockup#jobs'
+  get   'mockup/replies',           :to => 'mockup#replies'
+  get   'mockup/activity',          :to => 'mockup#activity'
+  get   'mockup/team',              :to => 'mockup#team'
+  get   'mockup/customize',         :to => 'mockup#customize'
+  get   'mockup/plugins',           :to => 'mockup#plugins'
 
   # TODO: Should be session or sessions
   get   'signin',                 :to => 'session#new'
