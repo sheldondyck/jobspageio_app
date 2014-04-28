fe26bb3d4ba3c0dffb46be3992820164aadedf98
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2014-04-28 09:29:04 -0300

    started to add some simple feature tests

---
+++ spec/features/mockups_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'Mockup smoke' do
+  scenario 'login fails' do
+    visit signin_path
+
+    fill_in 'Email', :with => 'foo@bar.com'
+    fill_in 'Password', :with => 'foobar1'
+    click_button 'Sign In'
+
+    #save_and_open_page
+    expect(page).to have_text('Welcome back')
+  end
+
+  scenario 'login success' do
+    visit signin_path
+
+    fill_in 'Email', :with => 'foo@bar.com'
+    fill_in 'Password', :with => 'foobar'
+    click_button 'Sign In'
+
+    #save_and_open_page
+    expect(page).to have_text('Sign Out')
+  end
+end
